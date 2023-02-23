eecfbbc1bc4027fd3cbafa0b596b69b552e5ace0
German Trushkov <germantrushkov@gmail.com>
2023-02-23 13:50:34 +0200

    add new files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-	
+	<div class="container">
+		<h1>Привет</h1>
+	</div>
 </template>
 
 <script>
+	export default{
+		data(){
+			return{
 
+			};
+		},
+	};
 </script>
 
 <style scoped lang="scss">
