82930aa91f1988cdfeadf4ad5e6d3f5ce3c66bf6
German Trushkov <germantrushkov@gmail.com>
2023-02-24 10:16:22 +0200

    add new components

--- src/App.vue
+++ src/App.vue
@@ -8,199 +8,16 @@
 			<span class="blue-line"></span>
 		</header>
 
-		<section id="first-section">
-			<div class="dream-work">
-				<div class="google-img">
-					<img src="./img/Google.jpg" alt="photo-google">
-				</div>
-				<div class="google-txt">
-					<p>Работа мечты компания Google</p>
-					<p>что из себя представляет работа в google?</p>
-					<p>
-						Для многих людей Google представляется большим зданием с 
-						прозрачными лоджиями и окнами от пола до потолка, которое усеянно
-						уютными и красивыми креслами/столами. От представления кафетерия в этом здании, 
-						мечты становтся еще красочнее, еда пестрит излишествами и деликатесами
-						и все это бесплатно. В окружении только доброжелательные, невероятно умные и
-						весёлые коллеги-друзья. Что же здесь правда?
-					</p>
-					<div class="google-btn">
-						<button>Читать далее</button>
-					</div>
-				</div>
-			</div>
-		</section>
-		
+		<SectionFirst />
+
 		<div class="last-published">
 			<p>Последние опубликованные</p>
 			<span class="blue-line-half"></span>
 		</div>
 		
-		<section id="second-section">
-			<!--------------------------------------------1-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/difficult-lang.jpeg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>Какие языки программирования ненавидят больше и почему?</p>
-					<p>
-						Авторитетный веб-ресурс Stack Overflow решил заняться опросом в отношении
-						языков программирования. В этот раз в центре внимания нелюбимые языки.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------2-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/camera.jpg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>Надо ли заклеивать камеру на ноутбуке?</p>
-					<p>
-						Заклеивать камеру ноутбука - паранойя или эффективное средство защититься от хакеров?
-						Мы рассмотрим аспекты за и против и ответим на этот вездесущий вопрос.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------3-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/future.jpg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>5 будущих профессий для IT специалистов</p>
-					<p>
-						Профессии постоянно пополняются и IT сфера вовсе не исключение. 
-						Мы подготовили список из 5 возможнных будущих профессий, к которым можно
-						подготовиться уже сейчас.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------4-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/python.jpg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>ИИ на Python для распознавания голоса</p>
-					<p>
-						На Python можно строить как простые программы, так и сложные 
-						ИИ системы. В данной статье мы покажем как реализовать распознавание
-						голоса и выполнение различных команд.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------5-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/git.jpg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>Изучение Git на реальных примерах</p>
-					<p>
-						Git - это очень полезная система управления версиями, которая позволяет
-						разработчикам эффективнее работать вместе. В статье вкратце на примерах,
-						вы изучите основные моменты Git.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------6-------------------------------------------->
-			<div class="news-list">
-				<div class="news-img">
-					<img src="./img/javascript.jpg" alt="photo-google">
-				</div>
-				<div class="news-txt">
-					<p>Почему JavaScript очень крутой язык программирования?</p>
-					<p>
-						Язык JavaScript за последние года стал мега популярным и завоевал 
-						огромное доверие среди разработчиков. Почему он так крут?
-						Узнаем в этой статье.
-					</p>
-				</div>
-				<div class="news-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-		</section>
+		<SectionSecond />
 
-		<section id="third-section">
-			<!--------------------------------------------1-------------------------------------------->
-			<div class="another-news">
-				<div class="another-img">
-					<img src="./img/frontend.png" alt="photo-google">
-				</div>
-				<div class="another-txt">
-					<p>Front-end</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
-					</p>
-				</div>
-				<div class="another-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------2-------------------------------------------->
-			<div class="another-news">
-				<div class="another-img">
-					<img src="./img/swift.jpg" alt="photo-google">
-				</div>
-				<div class="another-txt">
-					<p>Swift</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
-					</p>
-				</div>
-				<div class="another-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------3-------------------------------------------->
-			<div class="another-news">
-				<div class="another-img">
-					<img src="./img/V1.jpg" alt="photo-google">
-				</div>
-				<div class="another-txt">
-					<p>Python</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
-					</p>
-				</div>
-				<div class="another-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-			<!--------------------------------------------4-------------------------------------------->
-			<div class="another-news">
-				<div class="another-img">
-					<img src="./img/angular.png" alt="photo-google">
-				</div>
-				<div class="another-txt">
-					<p>Angular</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
-					</p>
-				</div>
-				<div class="another-btn">
-					<button>Далее</button>
-				</div>
-			</div>
-		</section>
+		<SectionThird />
 
 		<div class="info">
 			<p>Информация</p>
@@ -239,11 +56,15 @@
 </template>
 
 <script>
+	import SectionFirst from './components/SectionFirst.vue';
+	import SectionSecond from './components/SectionSecond.vue';
+	import SectionThird from './components/SectionThird.vue';
+
 	export default{
-		data(){
-			return{
-				
-			};
+		components: {
+			SectionFirst,
+			SectionSecond,
+			SectionThird,
 		},
 	};
 </script>
---
+++ src/components/SectionThird.vue
@@ -0,0 +1,72 @@
+<template>
+	<section id="third-section">
+		<!--------------------------------------------1-------------------------------------------->
+		<div class="another-news">
+			<div class="another-img">
+				<img src="../img/frontend.png" alt="photo-google">
+			</div>
+			<div class="another-txt">
+				<p>Front-end</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
+				</p>
+			</div>
+			<div class="another-btn">
+				<button>Далее</button>
+			</div>
+		</div>
+		<!--------------------------------------------2-------------------------------------------->
+		<div class="another-news">
+			<div class="another-img">
+				<img src="../img/swift.jpg" alt="photo-google">
+			</div>
+			<div class="another-txt">
+				<p>Swift</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
+				</p>
+			</div>
+			<div class="another-btn">
+				<button>Далее</button>
+			</div>
+		</div>
+		<!--------------------------------------------3-------------------------------------------->
+		<div class="another-news">
+			<div class="another-img">
+				<img src="../img/V1.jpg" alt="photo-google">
+			</div>
+			<div class="another-txt">
+				<p>Python</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
+				</p>
+			</div>
+			<div class="another-btn">
+				<button>Далее</button>
+			</div>
+		</div>
+		<!--------------------------------------------4-------------------------------------------->
+		<div class="another-news">
+			<div class="another-img">
+				<img src="../img/angular.png" alt="photo-google">
+			</div>
+			<div class="another-txt">
+				<p>Angular</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, id minima.
+				</p>
+			</div>
+			<div class="another-btn">
+				<button>Далее</button>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
